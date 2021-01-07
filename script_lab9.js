d830980029bbc26df1b63520590cd40782290518
Kate <shunaev80@mail.ru>
2021-01-07 19:02:37 +0300

    file js

--- script_lab9.js
+++ script_lab9.js
@@ -1,12 +1,30 @@
-function z1(last_name, first_name, group){
-	this.first_name = first_name;
-	this.last_name = last_name;
-	this.group = group;
-}
-{
+function z1(){
+	console.log('Zadacha 1')
 	let student = {
-	group: 201,
-	last_name: "Иванов",
-	first_name: "Иван"
+		group: 201,
+		last_name: "Иванов",
+		first_name: "Иван"
 	};
+	console.log('Свойства объекта: ');
+	for (key in student) 
+	{
+  	console.log(key);
+	}
+	console.log(`Студент ${student.first_name} ${student.last_name} учится в ${student.group} группе`);
+}
+
+function z2(){
+	console.log('Zadacha 2')
+}
+
+function z3(){
+	console.log('Zadacha 3')
+}
+
+function z4(){
+	console.log('Zadacha 4')
+}
+
+function z5(){
+	console.log('Zadacha 5')
 }
