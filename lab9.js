3ee165708e90f247a3c88480ce06faaf367c57e1
Kate <shunaev80@mail.ru>
2021-01-08 02:29:47 +0300

    final file js

--- lab9.js
+++ lab9.js
@@ -1,43 +1,47 @@
-function z1(){
-	console.log('Zadacha 1')
-	let student = {
-		group: 201,
-		last_name: "Иванов",
-		first_name: "Иван"
-	};
-	console.log('Свойства объекта: ');
-	for (key in student) 
-	{
-  	console.log(key);
-	}
-	console.log(`Студент ${student.first_name} ${student.last_name} учится в ${student.group} группе`);
+
+
+let student = {
+    group: 201,
+    last_name: "Иванов",
+    first_name: "Иван"
+    };
+
+function arrayCut(arr)
+{
+    for(var i = 0; i <= arr.length+2; i++) {
+        console.log(arr.length,"Осталось",arr,'  i-',i);
+        arr.pop();
+
+    }
 }
 
-function z2(){
-	console.log('Zadacha 2')
-	function arrayCut(arr) {
-		for (let i = 0; i <= arr.length; i++) {
-			console.log(arr.length,"Осталось",arr,'  i-',i);
-        arr.pop();
-		}
-	}
-	let array = [0,1,2,3];
+function z1()
+{
+    console.log('group :',student.group);
+    console.log('name :',student.name);
+    console.log('last_name :',student.last_name);
+
+}    
+
+function z2()
+{
+    let array = [0,1,2,3];
     arrayCut(array);
 }
 
-function z3(){
-	console.log('Zadacha 3')
-	var word = prompt("введите слово");
+function z3()
+{
+    var word = prompt("введите слово");
     isPalindrome(word);
 }
 
-function z4(){
-	console.log('Zadacha 4')
-	var matr = [[1,2,3],[4,5,6],[7,8,9]];
+
+function z4()
+{
+    var matr = [[1,2,3],[4,5,6],[7,8,9]];
     var sc = 10;
     scalarMultiplication(sc,matr);
 }
-
 
 function isPalindrome(word)
 {
@@ -52,7 +56,9 @@ function isPalindrome(word)
         if(word_copy[i] != word_reverse[i])
         {
            a = false
+           //console.log('not P')
         }
+       //console.log('i- ',i)
     }
     console.log(a);
 }
