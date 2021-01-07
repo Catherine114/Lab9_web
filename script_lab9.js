6f260771f4c4eabc611906809c78b1c710094046
Kate <shunaev80@mail.ru>
2021-01-07 16:01:05 +0300

    file js z1

---
+++ script_lab9.js
@@ -0,0 +1,12 @@
+function z1(last_name, first_name, group){
+	this.first_name = first_name;
+	this.last_name = last_name;
+	this.group = group;
+}
+{
+	let student = {
+	group: 201,
+	last_name: "Иванов",
+	first_name: "Иван"
+	};
+}
